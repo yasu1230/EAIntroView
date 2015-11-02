3e3182a798b2e1cb7ae8fde7fce05ce7e30fb1ad
Yasuyuki Inoue <yasu@net-walker.jp>
2015-11-03 02:21:23 +0900

    Carthage compatible

--- EAIntroView/EAIntroView-framework.h
+++ EAIntroView/EAIntroView-framework.h
@@ -6,16 +6,9 @@
 //  Copyright © 2015年 Y.Inoue. All rights reserved.
 //
 
-#ifndef EAIntroView_framework_h
-#define EAIntroView_framework_h
-
-#import <UIKit/UIKit.h>
-
 //! Project version number for EAIntroView.
 FOUNDATION_EXPORT double EAIntroViewVersionNumber;
 
 //! Project version string for EAIntroView.
 FOUNDATION_EXPORT const unsigned char EAIntroViewVersionString[];
 
-
-#endif /* EAIntroView_framework_h */
